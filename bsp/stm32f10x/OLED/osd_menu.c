a03943f0ed051aa6d0d487ef5ccef89626dacf85
pigeon0411 <pigeon0411@qq.com>
2016-06-20 21:29:19 +0800

    333  eb06x--> modify

---
+++ bsp/stm32f10x/OLED/osd_menu.c
@@ -0,0 +1,535 @@
+#include <board.h>
+#include <rtthread.h>
+
+#ifdef  RT_USING_COMPONENTS_INIT
+#include <components.h>
+#endif  /* RT_USING_COMPONENTS_INIT */
+
+#include "includes.h"
+#include "oled.h"
+#include "bmp.h"
+#include "osd_menu.h"
+
+#include "key_ctl.h"
+
+
+u8 filter_mode_value;
+enum KEY_STATE_TYPE recorder_work_state;
+u8 ssd_state;
+u8 video_sd_hd_mode;
+
+
+
+void osd_clear_x_y(u8 x_start,u8 x_end,u8 y,u8 sizef)
+{
+	u8 ii;
+
+	for(ii=x_start;ii<x_end;)
+	{
+		OLED_ShowString(ii,y," ",sizef);
+		ii+=sizef;
+	}
+}
+
+
+
+
+
+#define	OSD_LINE1_X_POS		0
+#define	OSD_LINE4_Y_POS		6
+
+
+
+void osd_val_disp(u8 *data,u8 cnt)
+{
+	//u8 i;
+
+	//u8 buff[12];
+
+	//if(cnt > 10)
+	//	return;
+	//
+	//for(i=0;i<cnt;i++)
+	//{
+	//	buff[i]=data[i];
+	//}
+	//buff[i] = '\0';
+	OLED_ShowString(OSD_VAL_START_ADDR_X,OSD_VAL_START_ADDR_Y,data,16);  
+
+}
+
+void osd_line2_val_disp_clear(void)
+{
+
+	//osd_clear_x_y(OSD_VAL_START_ADDR_X,OSD_VAL_START_ADDR_X+8*7,OSD_VAL_START_ADDR_Y,16);
+	OLED_Clear_line(0,OSD_LINE2_Y_POS,8);
+	OLED_Clear_line(0,OSD_LINE2_Y_POS+1,8);
+}
+
+void osd_line1_val_disp_clear(void)
+{
+
+	//osd_clear_x_y(OSD_VAL_START_ADDR_X,OSD_VAL_START_ADDR_X+8*7,OSD_VAL_START_ADDR_Y,16);
+	OLED_Clear_line(0,0,16);
+	OLED_Clear_line(0,0+1,16);
+}
+
+
+extern u8 battery_power;
+extern u8 video_sd_hd_mode;
+
+void osd_line2_disp(u8 x)
+{
+static u8 battery_power_pre=0xff;
+
+
+
+	if(battery_power_pre==0xff || battery_power_pre!=battery_power )
+	{
+		osd_line2_val_disp_clear();
+
+
+		osd_battery_mode_disp(battery_power,OSD_LINE2_X_16_POS,OSD_LINE2_Y_16_POS);
+
+		osd_sd_hd_mode_disp(video_sd_hd_mode,OSD_LINE2_X_16_POS+88,OSD_LINE2_Y_16_POS);
+
+		battery_power_pre = battery_power;
+
+	}
+}
+
+
+void osd_line_2_disp_item_clear(u8 select)
+{
+	if(select>3)
+		return;
+
+	u8 xstart;//,xend;
+	
+	//osd_line2_val_disp_clear();	
+
+	switch(select)
+	{
+	case 1:
+
+		break;
+	case 2:
+		break;
+	case 3:// filter
+		//osd_para_mode_disp(cam_para_mode);
+		//osd_iris_mode_disp(iris_mode);
+		break;
+	default:break;
+
+	}
+	
+}
+
+
+extern u8 osd_mid_buff[30];
+extern u8 key_val_buffer_cnt;
+extern u8 osd_mid_str_buff[10];
+
+void osd_set_para_disp_line_1(u8 item)
+{
+
+}
+
+
+const u8*osd_line1_string={"Mspectrum Imager"};
+
+
+void osd_line1_disp(u8 x)
+{
+	//osd_val_disp(osd_mid_buff,key_val_buffer_cnt);
+
+	OLED_ShowString(0,OSD_VAL_START_ADDR_Y,(u8*)osd_line1_string,16);  
+
+	
+}
+
+
+void OLED_Clear_line(u8 x,u8 y,u8 sizef)  
+{  
+	u8 i,n;		
+
+	i = y;
+	
+ 
+	OLED_WR_Byte (0xb0+i,OLED_CMD);    //¨¦¨¨??¨°3¦Ì??¡¤¡ê¡§0~7¡ê?
+	OLED_WR_Byte (0x00,OLED_CMD);      //¨¦¨¨????¨º??????a¨¢D¦Ì¨ª¦Ì??¡¤
+	OLED_WR_Byte (0x10,OLED_CMD);      //¨¦¨¨????¨º??????a¨¢D??¦Ì??¡¤   
+	for(n=0;n<128;n++)OLED_WR_Byte(0,OLED_DATA); 
+}
+
+
+
+void osd_para_mode_disp(u8 mode)
+{
+	u8 *mstr="Mode";
+	u8 pp;
+
+	if(mode > 8)
+		return;
+	OLED_ShowString(OSD_PARA_MODE_X_START,OSD_PARA_MODE_Y,mstr,16); 
+	
+	pp = mode+0x31;
+	OLED_ShowString(OSD_PARA_MODE_X_START+32,OSD_PARA_MODE_Y,&pp,16); 
+}
+
+extern u8 cam_filter_mode;
+
+
+
+
+void osd_battery_mode_disp(u8 data,u8 x,u8 y)
+{
+	static u8 tmp = 1;
+
+	if(tmp)
+		{
+		tmp = 0;
+	if(data>100)
+		data = 100;
+	
+	OLED_ShowString(x,y,"Batt:   %",16); 
+	OLED_ShowNum(x+40,y,data,3,16); 
+		tmp = 1;
+		}
+		return;
+
+}
+
+void osd_battery_val_disp(u8 data,u8 x,u8 y)
+{
+	if(data>100)
+		data = 100;
+	
+	OLED_ShowNum(x+40,y,data,3,16); 
+	
+		return;
+
+}
+
+
+void osd_sd_hd_mode_disp_clear(u8 data,u8 x,u8 y)
+{
+
+		OLED_ShowString(x,y,"     ",16); 
+
+	return;
+}
+
+
+void osd_sd_hd_mode_disp(u8 data,u8 x,u8 y)
+{
+	osd_sd_hd_mode_disp_clear(data,x,y);
+
+	if(data)
+	{
+		OLED_ShowString(x,y,"HD/SD",16); 
+
+		}
+	else
+		{
+		OLED_ShowString(x,y+1,"HD/SD",8); 
+
+		}
+	return;
+}
+
+
+
+
+
+#define	PIXCEL_NUM_PER_FONT_16		8
+
+const u8 *filter_mode_string[]={
+	//{"    "},
+		{" UV "},
+		{" BL "},
+		{" GR "},
+		{" IR "},
+
+};
+
+void filter_mode_disp_clear(u8 x,u8 y,u8 mode)
+{
+	if(mode>3)
+		mode=3;
+		
+	OLED_ShowString(x+mode*16,y,(u8*)"  ",16);
+
+}
+
+void filter_mode_disp(u8 x,u8 y,u8 mode)
+{
+	if(mode>3)
+		mode=3;
+	u8 i = 16;
+
+
+	for(u8 k=0;k<4;k++)
+	{
+		if(k == mode)
+			i = 16;
+		else
+			i = 8;
+
+		if(i==16)
+			OLED_ShowString(x+k*32,y,(u8*)filter_mode_string[k],i);
+		else
+			OLED_ShowString(x+k*32,y+1,(u8*)filter_mode_string[k],i);
+			
+	}
+}
+
+
+
+
+extern u8 filter_mode_value;
+
+
+void osd_line3_disp(u8 select)
+{
+
+	static u8 filter_mode_value_pre = 0xff;
+
+	if(filter_mode_value_pre == 0xff || filter_mode_value != filter_mode_value_pre)
+	{
+		OLED_Clear_line(0,OSD_LINE3_Y_POS,16);
+		OLED_Clear_line(0,OSD_LINE3_Y_POS+1,16);
+
+		filter_mode_disp(0,OSD_LINE3_Y_POS,filter_mode_value);
+		filter_mode_value_pre = filter_mode_value;
+	}
+}
+
+
+void osd_line3_disp_clear_line(void)
+{
+	
+	OLED_Clear_line(0,OSD_LINE3_Y_POS,16);
+	OLED_Clear_line(0,OSD_LINE3_Y_POS+1,16);
+
+}
+
+
+#define	OSD_LINE_4_LITTLE_START_X	0
+#define	OSD_LINE_4_LITTLE_START_Y	(6)
+
+#define	OSD_LINE_4_FONT_PIXEL_X		8
+
+
+u8 line4_buff[16]={0};
+
+
+const u8 ssd_state_msg[]={
+
+	"SSD"};
+
+
+
+extern enum KEY_STATE_TYPE recorder_work_state;
+
+extern u8 record_state;
+extern u8 ssd_state;
+
+extern enum KEY_STATE_TYPE recorder_work_state;
+u8 *recorder_state_msg[]={
+		{"         "},
+	{"Recording"},
+		{"Play"},
+		{"Pause"},
+
+	{"Next"},
+		
+	{"Pause"},
+};
+
+
+
+enum KEY_STATE_TYPE recorder_work_state_pre = KEY_STATE_INIT;
+
+u8 ssd_state_pre = 0xff;
+void osd_line4_disp(u8 chn)
+{
+
+    if(recorder_work_state_pre==KEY_STATE_INIT || recorder_work_state_pre!=recorder_work_state
+        ||ssd_state_pre==0xff||ssd_state_pre!=ssd_state)
+    {
+        OLED_Clear_line(0,6,16);
+        OLED_Clear_line(0,7,16);
+
+        memset(line4_buff,0,16);
+
+        //if(record_state)
+        	strcpy((char *)line4_buff,(const char*)recorder_state_msg[recorder_work_state]);
+        if(ssd_state)
+        	strcpy((char *)line4_buff+10,(const char*)ssd_state_msg);
+
+
+        OLED_ShowString(0,6,line4_buff,16);
+
+        recorder_work_state_pre = recorder_work_state;
+        ssd_state_pre = ssd_state;
+    }
+
+    
+}
+
+
+void osd_line_little_4_disp(u8 chn)
+{
+	OLED_Clear_line(0,6,16);
+	OLED_Clear_line(0,7,16);
+	
+	strcpy(line4_buff,recorder_state_msg[recorder_work_state]);
+	OLED_ShowString(0,6,line4_buff,16);
+
+}
+
+void osd_line_little_4_disp_item_clear(u8 select)
+{
+
+}
+
+
+void osd_line_1to4_all_disp(void)
+{
+	static u8 tmp = 1;
+
+	if(tmp)
+		{
+		tmp = 0;
+
+	osd_line1_disp(1);	
+	osd_line2_disp(1);
+	osd_line3_disp(2);	
+	osd_line4_disp(0);
+tmp = 1;
+		}
+}
+
+
+void rt_osd_thread_entry(void* parameter)
+{
+
+	while(1)
+	{
+			
+#if 0
+
+		if(menu_normal_flag)
+		{
+			if(cursor_ver_pos == 0)
+			{
+				osd_sd_hd_mode_disp(video_sd_hd_mode,OSD_LINE2_X_16_POS+88,OSD_LINE2_Y_16_POS);
+				rt_thread_delay(600);
+				osd_sd_hd_mode_disp_clear(video_sd_hd_mode,OSD_LINE2_X_16_POS+88,OSD_LINE2_Y_16_POS);
+			}
+			else if(cursor_ver_pos == 1)
+			{
+				filter_mode_disp(0,OSD_LINE3_Y_POS,filter_mode_value);
+				rt_thread_delay(600);
+				
+				filter_mode_disp_clear(0,OSD_LINE3_Y_POS,filter_mode_value);
+			}
+			rt_thread_delay(500);
+
+		}
+		else
+		{
+
+			osd_battery_val_disp(video_sd_hd_mode,OSD_LINE2_X_16_POS,OSD_LINE2_Y_16_POS);
+			
+		}
+#endif		
+		
+		//osd_battery_mode_disp(battery_power,OSD_LINE2_X_16_POS,OSD_LINE2_Y_16_POS);
+		//osd_line4_disp(1);
+
+		//osd_line_1to4_all_disp();
+		osd_line2_disp(1);
+		osd_line3_disp(2);	
+		osd_line4_disp(0);
+
+		rt_thread_delay(500);
+	}
+
+}
+
+void osd_init(void)
+{
+	OLED_Init();			//3?¨º??¡¥OLED  
+	OLED_Clear()  	; 
+	osd_line_1to4_all_disp();
+
+	rt_thread_t init_thread;
+
+
+	init_thread = rt_thread_create("osd",
+								   rt_osd_thread_entry, RT_NULL,
+								   512, 10, 5);
+	if (init_thread != RT_NULL)
+		rt_thread_startup(init_thread);
+
+
+}
+
+void osd_test(void)
+{	u8 t;
+		OLED_Init();			//3?¨º??¡¥OLED  
+		OLED_Clear()  	; 
+	
+		t=' ';
+		OLED_ShowCHinese(0,0,0);//?D
+				OLED_ShowCHinese(18,0,1);//?¡ã
+		OLED_ShowCHinese(36,0,2);//?¡ã
+		OLED_ShowCHinese(54,0,3);//¦Ì?
+		OLED_ShowCHinese(72,0,4);//¡Á¨®
+		OLED_ShowCHinese(90,0,5);//??
+		OLED_ShowCHinese(108,0,6);//??
+	while(1) 
+	{		
+		OLED_Clear();
+		OLED_ShowCHinese(0,0,0);//?D
+		OLED_ShowCHinese(18,0,1);//?¡ã
+		OLED_ShowCHinese(36,0,2);//?¡ã
+		OLED_ShowCHinese(54,0,3);//¦Ì?
+		OLED_ShowCHinese(72,0,4);//¡Á¨®
+		OLED_ShowCHinese(90,0,5);//??
+		OLED_ShowCHinese(108,0,6);//??
+		OLED_ShowString(6,3,"0.96' OLED TEST",16);
+
+				OLED_ShowString(6,4,"0.96' OLED TEST",16);
+		OLED_ShowString(0,5,"0.96' OLED TEST",16);
+
+		OLED_ShowString(0,6,"0.96' OLED TEST",16);
+		OLED_ShowString(1,7,"0.96' OLED TEST",16);
+
+		OLED_ShowString(6,3,"0.96' OLED TEST",16);
+
+		//OLED_ShowString(8,2,"ZHONGJINGYUAN");  
+	 //	OLED_ShowString(20,4,"2014/05/01");  
+		OLED_ShowString(0,6,"ASCII:",16);  
+		OLED_ShowString(63,6,"CODE:",16);  
+		OLED_ShowChar(48,6,t,16);//??¨º?ASCII¡Á?¡¤?	   
+		t++;
+		if(t>'~')t=' ';
+		OLED_ShowNum(103,6,t,3,16);//??¨º?ASCII¡Á?¡¤?¦Ì????¦Ì 	
+		rt_thread_delay(1000*3);
+
+
+		OLED_DrawBMP(0,0,128,8,BMP1);  //¨ª?????¨º?(¨ª?????¨º?¨¦¡Â¨®?¡ê?¨¦¨²3¨¦¦Ì?¡Á?¡À¨ª??¡ä¨®¡ê??¨¢??¨®????¨¤????¡ê?FLASH????8K¨°???¨¦¡Â¨®?)
+		rt_thread_delay(1000*3);
+
+		OLED_DrawBMP(0,0,128,8,BMP1);
+		rt_thread_delay(1000*3);
+
+
+	}	
+	
+}
+
+
---
+++ bsp/stm32f10x/applications/key_ctl.c
@@ -0,0 +1,636 @@
+#include <board.h>
+#include <rtthread.h>
+
+#ifdef  RT_USING_COMPONENTS_INIT
+#include <components.h>
+#endif  /* RT_USING_COMPONENTS_INIT */
+
+#include "includes.h"
+#include "osd_menu.h"
+#include "key_ctl.h"
+
+
+
+
+#define	PD_ZOOM_FOCUS_STOP		0
+#define	PD_ZOOM_TELE_CMD		1
+#define	PD_ZOOM_WIDE_CMD		2
+#define	PD_FOCUS_FAR_CMD		3
+#define	PD_FOCUS_NEAR_CMD		4
+
+
+
+void pelcod_call_pre_packet_send(u8 val);
+
+
+
+#define	KEY_PORT1		GPIOA
+#define	KEY_PORT2		GPIOB
+
+void key_pin_init(void)
+{
+	GPIO_InitTypeDef GPIOD_InitStructure;
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+	
+		
+	GPIOD_InitStructure.GPIO_Pin = GPIO_Pin_15;
+	GPIOD_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+	GPIOD_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOB, &GPIOD_InitStructure); 
+
+	
+	GPIOD_InitStructure.GPIO_Pin = GPIO_Pin_6|GPIO_Pin_7|GPIO_Pin_8|GPIO_Pin_9;
+	GPIO_Init(GPIOC, &GPIOD_InitStructure); 
+
+
+}
+
+
+
+
+extern rt_sem_t	uart1_sem;
+
+rt_err_t rs485_recieve_check(u8 val)
+{
+
+	
+	if(rt_sem_take(uart1_sem, 30) == RT_EOK)
+    {
+		if (command_analysis()) 
+		{
+            switch(command_byte)
+		    {
+			 	case 0x11://call preset point
+
+					if(Rocket_fir_data == val)
+						return RT_EOK;
+					break;
+
+             	default:
+				break;
+	   	    }
+
+		}
+	}
+	return RT_ERROR;
+
+}
+
+
+u8 cmd_buff[7];
+
+rt_sem_t rs485_return_sem;
+
+extern rt_err_t rs485_send_data(u8* data,u16 len);
+
+void pelcod_call_pre_packet_send(u8 val)
+{
+	u8 cnt;
+	cmd_buff[0] = 0xff;
+	cmd_buff[1] = 0xff;
+	cmd_buff[2] = 0;
+	cmd_buff[3] = 0x07;
+	cmd_buff[4] = 0;
+	cmd_buff[5] = val;
+	
+	cmd_buff[6] = cmd_buff[1] + cmd_buff[2] + cmd_buff[3] + cmd_buff[4] + cmd_buff[5];
+	rs485_send_data(cmd_buff,7);
+
+//	cnt=3;
+//	while(cnt--)
+//	{
+//		if(RT_EOK == rs485_recieve_check(val))
+//			break;
+//		else
+//			rs485_send_data(cmd_buff,7);
+//	}
+}
+
+
+void pelcod_set_pre_packet_send(u8 val)
+{
+	u8 cnt;
+	cmd_buff[0] = 0xff;
+	cmd_buff[1] = 0xff;
+	cmd_buff[2] = 0;
+	cmd_buff[3] = 0x03;
+	cmd_buff[4] = 0;
+	cmd_buff[5] = val;
+	
+	cmd_buff[6] = cmd_buff[1] + cmd_buff[2] + cmd_buff[3] + cmd_buff[4] + cmd_buff[5];
+	rs485_send_data(cmd_buff,7);
+
+//	cnt=3;
+//	while(cnt--)
+//	{
+//		if(RT_EOK == rs485_recieve_check(val))
+//			break;
+//		else
+//			rs485_send_data(cmd_buff,7);
+//	}
+}
+
+//val: 0,open; 1,close
+void pelcod_open_close_packet_send(u8 val)
+{
+	u8 cnt;
+	cmd_buff[0] = 0xff;
+	cmd_buff[1] = 0xff;
+	if(val)//close
+		cmd_buff[2] = 0x04;
+	else
+		cmd_buff[2] = 0x02;
+	cmd_buff[3] = 0;
+	cmd_buff[4] = 0;
+	cmd_buff[5] = 0;
+	
+	cmd_buff[6] = cmd_buff[1] + cmd_buff[2] + cmd_buff[3] + cmd_buff[4] + cmd_buff[5];
+	rs485_send_data(cmd_buff,7);
+
+//	cnt=3;
+//	while(cnt--)
+//	{
+//		if(RT_EOK == rs485_recieve_check(val))
+//			break;
+//		else
+//			rs485_send_data(cmd_buff,7);
+//	}
+}
+
+//cmd,0,stop; 1,tele,2wide; 3,far,4,near
+void pelcod_zf_packet_send(u8 cmd,u8 zfspeed)
+{
+	u8 cnt;
+	
+	u8 cmd_buff_private[7];
+	cmd_buff_private[0] = 0xff;
+	cmd_buff_private[1] = domeNo;
+
+	
+	switch(cmd)
+	{
+	case 1:
+		cmd_buff_private[3] = 0x20;
+		cmd_buff_private[2] = 0;
+		break;
+	case 2:
+		cmd_buff_private[3] = 0x40;
+		cmd_buff_private[2] = 0;
+		break;
+	case 3:
+		cmd_buff_private[3] = 0x00;
+		cmd_buff_private[2] = 0x01;//
+		break;
+	case 4:
+		cmd_buff_private[3] = 0x80;
+		cmd_buff_private[2] = 0;
+		break;
+	case 0:
+		cmd_buff_private[3] = 0x00;
+		cmd_buff_private[2] = 0;
+		break;
+	}
+	
+	cmd_buff_private[4] = 0;
+	cmd_buff_private[5] = 0;
+	
+	cmd_buff_private[6] = cmd_buff_private[1] + cmd_buff_private[2] + cmd_buff_private[3] + cmd_buff_private[4] + cmd_buff_private[5];
+	rs485_send_data(cmd_buff_private,7);
+}
+
+
+
+u16 KeyPre = 0;//  上次按键的状态
+u16 KeyCurrent = 0;//  本次按键的状态
+u16 KeyStbLast = 0;// 经滤波后的上次按键的状态
+u16 KeyStb = 0;//  经滤波后的本次按键的状态
+u16 KeyAvlUp = 0;// 按键抬起有效结果寄存器
+u16 KeyAvlDown = 0;//  按键按下有效结果寄存器
+
+#define	KEY_DOWN_STATE		0
+
+u16 key_val_table[] = {KEY_LEFT_PIN,KEY_RIGHT_PIN,KEY_UP_PIN,KEY_DOWN_PIN,KEY_SET_PIN};
+
+
+
+
+#define		KEY_DEFAULT		0X1F00
+
+
+u16 key_pre = 0;
+u16 key_pre2 = 0;
+
+
+
+#define	KEY_PORT1		GPIOA
+#define	KEY_PORT2		GPIOB
+#define	KEY_PORT3		GPIOC
+
+
+enum key_type
+{
+	KEY_NONE,
+		KEY_FOCUS_PLUS, 
+		
+		KEY_ZOOM_SUB,	
+
+	
+	KEY_ZOOM_PLUS,	
+		KEY_FOCUS_SUB,	
+	
+	KEY_MODE,	
+};
+
+#define	key_to_long(val)	(val|0x9000)
+#define	key_to_release(val)	(val|0x8000)
+
+
+u32 key_merge(void)
+{
+	u32 data = 0;
+
+	u32 key_tmp;
+	
+	data = GPIO_ReadInputData(KEY_PORT3);	
+		
+	key_tmp = (data>>6)&0x000f;//0-1
+
+
+	return key_tmp;
+}
+
+#define	KEY_NUMS_MAX	4
+
+
+u32 press_continue_cnt = 0;
+u8 continue_motor_flag = 0;
+u16 motor_steps_cnt = 0;
+u8 press_long_flag = 0;
+
+u8 press_long_flag2 = 0;
+u32 press_continue_cnt2 = 0;
+u8 continue_motor_flag2 = 0;
+
+u8 key_combine_state = 0;
+
+static u16 key_check_1(void)
+{
+	u16 i;
+	u32 key_tmp;
+	static u32 long_press_cnt = 0;// 50ms
+	
+	key_tmp = key_merge();
+	for(i=0;i<KEY_NUMS_MAX;i++)
+	{
+		if(((key_tmp>>i)&0x0001)==0)
+		{
+			rt_thread_delay(40);
+
+			key_tmp = key_merge();
+
+			if(((key_tmp>>i)&0x0001)==0)
+			{
+				if(key_pre == i+1)
+				{
+
+					if(key_pre2 != KEY_NONE)
+					{
+						if(long_press_cnt>50)
+						{
+							if(press_long_flag)
+								return 0;
+							
+							press_long_flag = 1;
+							long_press_cnt=0;
+							key_pre = 0;
+
+							
+							return ((i+1)|0x9000);
+						}
+						else
+						{
+							long_press_cnt++;
+
+						}
+					}
+					else
+					{
+						key_pre = i+1;
+						return (key_pre);	
+
+					}
+
+				}
+				key_pre = i+1;
+				//return (i+1);
+				break;
+			}
+		}
+	}
+
+
+	if((key_pre && key_pre!=(i+1))||(key_pre && i>=KEY_NUMS_MAX))
+	{
+		i = key_pre|0x8000;
+		key_pre = 0;
+
+		if(press_continue_cnt>10)
+			i = 0;
+
+		if(press_long_flag)
+		{
+			press_long_flag = 0;
+			i =0;
+		}
+		press_continue_cnt = 0;
+		motor_steps_cnt = 0;
+			
+		continue_motor_flag = 0;
+		
+		return i;
+
+	}
+	return 0;
+}	
+
+
+static u16 key_check_2(void)
+{
+	u16 i=0;
+	u32 key_tmp;
+	static u32 long_press_cnt = 0;// 50ms
+	
+	key_tmp = GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_15);
+	
+
+	if(((key_tmp)&0x0001)==0)
+	{
+		rt_thread_delay(40);
+
+		key_tmp = GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_15);
+
+		i=0;
+		if(((key_tmp)&0x0001)==0)
+		{
+			if(key_pre2 == KEY_MODE)
+			{
+				if(long_press_cnt>40)
+				{
+					if(press_long_flag2)
+						return 0;
+					
+					press_long_flag2 = 1;
+					long_press_cnt=0;
+					key_pre2 = 0;
+
+					
+					return (KEY_MODE|0x9000);
+				}
+				
+				long_press_cnt++;
+
+			}
+			key_pre2 = KEY_MODE;
+			return (KEY_MODE);
+		}
+	}
+
+
+	if(key_pre2==KEY_MODE)
+	{
+		i = key_pre2|0x8000;
+		key_pre2 = 0;
+
+		if(press_continue_cnt2>10)
+			i = 0;
+
+		if(press_long_flag2)
+		{
+			press_long_flag2 = 0;
+			i =0;
+		}
+		press_continue_cnt = 0;
+			
+		continue_motor_flag2 = 0;
+		
+		return i;
+
+	}
+	return 0;
+}
+
+
+
+//返回0为无按键，返回非0值，则为对应的按键号
+static u16 key_ctl_check(void)
+{
+	key_check_2();
+
+	
+	return (key_check_1());
+	
+}
+
+
+
+void key_analyze_mode(u16 mode_val,u16 val)
+{
+
+	if(mode_val == key_to_release(KEY_MODE) ||(mode_val == KEY_MODE)||(mode_val == key_to_long(KEY_MODE)))
+	{
+		
+		switch(val)
+		{
+		case key_to_release(KEY_FOCUS_PLUS):
+			
+			pelcod_call_pre_packet_send(203);
+		
+			break;
+		case key_to_release(KEY_FOCUS_SUB):
+			
+			pelcod_call_pre_packet_send(204);
+		
+			break;
+		case key_to_release(KEY_ZOOM_PLUS):
+			pelcod_call_pre_packet_send(201);
+		
+			break;
+
+			
+		case key_to_release(KEY_ZOOM_SUB):
+			pelcod_call_pre_packet_send(202);
+		
+			break;
+		case key_to_long(KEY_FOCUS_PLUS):
+			
+			pelcod_call_pre_packet_send(207);
+		
+			break;
+		case key_to_long(KEY_FOCUS_SUB):
+			
+			pelcod_call_pre_packet_send(208);
+		
+			break;
+		case key_to_long(KEY_ZOOM_PLUS):
+			
+			pelcod_call_pre_packet_send(205);
+		
+			break;
+		case key_to_long(KEY_ZOOM_SUB):
+			pelcod_call_pre_packet_send(206);
+		
+			break;
+							
+		default:
+			break;
+		}
+
+		return;
+	}
+	
+	
+}
+
+
+void key_analyze_non_mode(u16 mode_val,u16 val)
+{
+
+	
+	if(mode_val == 0)
+	{
+		if(val > 0 && val < KEY_MODE)
+			{
+				switch(val)
+				{
+				case (KEY_FOCUS_PLUS):
+					pelcod_zf_packet_send(PD_FOCUS_FAR_CMD,0);
+					break;
+				case (KEY_FOCUS_SUB):
+					pelcod_zf_packet_send(PD_FOCUS_NEAR_CMD,0);
+					break;
+		
+				case (KEY_ZOOM_PLUS):
+					pelcod_zf_packet_send(PD_ZOOM_TELE_CMD,0);
+					break;
+				case (KEY_ZOOM_SUB):
+					pelcod_zf_packet_send(PD_ZOOM_WIDE_CMD,0);
+					break;
+				default:
+					break;
+				}
+		
+				return;
+		
+			}
+		
+			switch(val)
+			{
+			case key_to_release(KEY_FOCUS_PLUS):
+			case key_to_release(KEY_FOCUS_SUB):
+			case key_to_release(KEY_ZOOM_PLUS):
+			case key_to_release(KEY_ZOOM_SUB):
+				pelcod_zf_packet_send(PD_ZOOM_FOCUS_STOP,0);
+		
+				break;	
+				
+			default:
+				break;
+			}
+
+	}
+}
+
+
+static u16 key_ctl_check_all(void)
+{
+	u16 key_mode_tmp;
+	u16 key_tmp ;
+	static u16 key_tmp_bak =0xff;
+
+
+	key_mode_tmp = key_check_2();
+	key_tmp = key_check_1();
+
+	if(key_mode_tmp || press_long_flag2)
+	{
+		if(key_tmp)
+		{
+			if(press_long_flag2)
+			{
+				key_mode_tmp = key_to_long(KEY_MODE);
+
+			}
+			key_analyze_mode(key_mode_tmp,key_tmp);
+			
+		}
+
+		key_combine_state = 1;
+	}
+	else
+	{
+
+		if(key_tmp)
+		{
+			if(key_combine_state)
+			{	
+				if(key_tmp_bak != key_tmp && key_tmp_bak!=0xff)
+				{
+					key_combine_state = 0;
+
+				}
+				
+			}
+			else
+			{
+				key_analyze_non_mode(key_mode_tmp,key_tmp);
+			}
+
+			key_tmp_bak = key_tmp;
+
+		}
+	}
+	return 0;
+	
+}
+
+
+
+void rt_key_thread_entry(void* parameter)
+{
+
+	u16 k;
+
+	key_pin_init();
+	
+
+    while(1)
+	{
+
+        key_ctl_check_all();
+
+
+		
+		
+		rt_thread_delay(4);
+    }
+}
+
+
+
+
+int rt_key_ctl_init(void)
+{
+
+	
+    rt_thread_t init_thread;
+
+    init_thread = rt_thread_create("key",
+                                   rt_key_thread_entry, RT_NULL,
+                                   4096, 10, 5);
+    if (init_thread != RT_NULL)
+        rt_thread_startup(init_thread);
+
+    return 0;
+}
+
